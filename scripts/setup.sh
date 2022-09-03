5d8c9b306a731deefd69e90c0c0805786f70b9a0
ivangayton <ivangayton@gmail.com>
2022-09-03 09:45:01 +0900

    create database

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,10 +6,10 @@ then
     echo setting up pip3
 else
     echo it was not installed, probably installing
-    sudo apt install python3-pip -y
+    sudo apt install python3-pip -y >> provisioning/setup.log 2>> provisioning/error.log
 fi
 
-sudo apt install python3-venv -y
+sudo apt install python3-venv -y >> provisioning/setup.log 2>> provisioning/error.log
 
 if [ -d venv ]
 then
@@ -19,16 +19,28 @@ else
 fi
 
 echo creating and activating virtual environment
-pip3 install wheel
-pip3 install virtualenv
+pip3 install wheel >> provisioning/setup.log 2>> provisioning/error.log
+pip3 install virtualenv >> provisioning/setup.log 2>> provisioning/error.log
 source venv/bin/activate
-pip install wheel
+pip install wheel >> provisioning/setup.log 2>> provisioning/error.log
 
 echo installing and starting postgres from default Ubuntu repo
 echo as per https://www.digitalocean.com/community/tutorials/how-to-install-postgresql-on-ubuntu-22-04-quickstart
-sudo apt install -y postgresql postgresql-contrib
+sudo apt install -y postgresql postgresql-contrib libpq-dev >> provisioning/setup.log 2>> provisioning/error.log
 sudo systemctl start postgresql.service
 
+echo Creating postgres user fmtmk
+# TODO This is a pretty obvious password (it's md5 hashed).
+# Generate another one using bash:
+# echo -n passwordusername | md5sum
+# and paste the result into the following command
+# with the prefix 'md5' (the hash below begins with 540e)
+sudo -u postgres psql -c "CREATE ROLE fmtm WITH PASSWORD 'md5540e7aa2739a14c6f2e6f07fd09c67f1';"
+sudo -u postgres psql -c "ALTER ROLE fmtm WITH LOGIN"
+sudo -u postgres psql -c "ALTER ROLE fmtm WITH SUPERUSER"
+
+echo Creating database odm360
+sudo -u postgres psql -c "CREATE DATABASE fmtm WITH OWNER fmtm;"
 
 echo Done.
 echo To use the utilities here, activate the virtual environment with:
