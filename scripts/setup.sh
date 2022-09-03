c60368c0fd00ecd38f565cd49cf897b54306b436
ivangayton <ivangayton@gmail.com>
2022-09-03 09:46:29 +0900

    create database 2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,10 +6,10 @@ then
     echo setting up pip3
 else
     echo it was not installed, probably installing
-    sudo apt install python3-pip -y >> provisioning/setup.log 2>> provisioning/error.log
+    sudo apt install python3-pip -y >> setup.log 2>> error.log
 fi
 
-sudo apt install python3-venv -y >> provisioning/setup.log 2>> provisioning/error.log
+sudo apt install python3-venv -y >> setup.log 2>> error.log
 
 if [ -d venv ]
 then
@@ -19,14 +19,14 @@ else
 fi
 
 echo creating and activating virtual environment
-pip3 install wheel >> provisioning/setup.log 2>> provisioning/error.log
-pip3 install virtualenv >> provisioning/setup.log 2>> provisioning/error.log
+pip3 install wheel >> setup.log 2>> error.log
+pip3 install virtualenv >> setup.log 2>> error.log
 source venv/bin/activate
-pip install wheel >> provisioning/setup.log 2>> provisioning/error.log
+pip install wheel >> setup.log 2>> error.log
 
 echo installing and starting postgres from default Ubuntu repo
 echo as per https://www.digitalocean.com/community/tutorials/how-to-install-postgresql-on-ubuntu-22-04-quickstart
-sudo apt install -y postgresql postgresql-contrib libpq-dev >> provisioning/setup.log 2>> provisioning/error.log
+sudo apt install -y postgresql postgresql-contrib libpq-dev >> setup.log 2>> error.log
 sudo systemctl start postgresql.service
 
 echo Creating postgres user fmtmk
@@ -39,7 +39,7 @@ sudo -u postgres psql -c "CREATE ROLE fmtm WITH PASSWORD 'md5540e7aa2739a14c6f2e
 sudo -u postgres psql -c "ALTER ROLE fmtm WITH LOGIN"
 sudo -u postgres psql -c "ALTER ROLE fmtm WITH SUPERUSER"
 
-echo Creating database odm360
+echo Creating database fmtm
 sudo -u postgres psql -c "CREATE DATABASE fmtm WITH OWNER fmtm;"
 
 echo Done.
