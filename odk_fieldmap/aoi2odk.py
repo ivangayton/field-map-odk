4874371bae899d1222d2f5c1adbc1b8497bb56b9
ivangayton <ivangayton@gmail.com>
2022-09-06 15:08:21 +0900

    Individual form creation working

--- odk_fieldmap/aoi2odk.py
+++ odk_fieldmap/aoi2odk.py
@@ -6,31 +6,30 @@ import json
 from geo_utils import get_extent_bbox
 from geo_utils import make_centroids
 from overpass import query
+
 from osm2geojson import json2geojson
+from openpyxl import load_workbook
 
 
-def aoi2project(aoi):
-    """
-    Takes a GeoJSON file with an aoi polygon, creates
-    - A GeoJSON of all the OSM building polygons in it
-    - A GeoJSON of the centroids thereof
-    - 
-    """
-    pass
-
-def prep_form(form_id):
+def prep_form(workbookfile, AOIfile, task_id):
     """
     Modifies the base instance of an ODK xlsform to refer
     to a specific area and GeoJSON file of features.
+    N.B. Only works with OSM_Buildings_ODK_v_0-0-6.xlsx
     """
-    # static/forms/OSM_Buildings_ODK_v_0-0-6.xlsx
-    # Modify $survey.A9
-    # select_one_from_file BuildingsXXXX.geojson
-    # Modify $settings.A2 and B2
-    # FMTM_Buildings_XXXX
-
-
-
+    (path, ext) = os.path.splitext(workbookfile)
+    (AOIpath, AOIext) = os.path.splitext(AOIfile)
+    AOIbasename = os.path.splitext(os.path.basename(AOIfile))[0]
+    wb = load_workbook(filename = workbookfile)
+    surveyws = wb['survey']
+    settingws = wb['settings']
+    
+    settingws['A2'] = f'FMTM_{AOIbasename}_{task_id}'
+    settingws['B2'] = f'FMTM_{AOIbasename}_{task_id}'
+    surveyws['A9'] = (f'select_one_from_file '
+                      f'{AOIbasename}_{task_id}{ext}')
+    wb.save(f'{path}_{AOIbasename}_{task_id}{ext}')
+    
 def get_buildings(aoi_file):
     """
     Given a GeoJSON AOI polygon, returns a centroid for every
@@ -46,9 +45,25 @@ def get_buildings(aoi_file):
     overpass_url = ('https://overpass.kumi.systems'
                     '/api/interpreter')
     overpass_json = query(querystring, overpass_url)
-    
     return json2geojson(overpass_json)
 
+def aoi2project(AOIfile, formfile):
+    """
+    Takes a GeoJSON file with an aoi polygon, creates
+    - A GeoJSON of all the OSM building polygons in it
+    - A GeoJSON of the centroids thereof
+    - 
+    """
+    (AOIpath, ext) = os.path.splitext(AOIfile)
+    buildings = get_buildings(AOIfile)
+    buildings_file = AOIpath + '_buildings' + ext
+    with open(buildings_file, 'w') as bf:
+        json.dump(buildings, bf)
+    make_centroids(buildings_file)
+
+    for i in range(0, 5):
+        prep_form(formfile, AOIfile, i)
+    
 if __name__ == "__main__":
     """
     If run from CLI, attempts to convert a GeoJSON
@@ -57,11 +72,5 @@ if __name__ == "__main__":
     of the AOI.
     """
     AOIfile = sys.argv[1]
-    (AOIpath, ext) = os.path.splitext(AOIfile)
-    buildings = get_buildings(AOIfile)
-    buildings_file = AOIpath + '_buildings' + ext
-    with open(buildings_file, 'w') as bf:
-        json.dump(buildings, bf)
-    make_centroids(buildings_file)
-    
-    
+    formfile = sys.argv[2]
+    aoi2project(AOIfile, formfile)
