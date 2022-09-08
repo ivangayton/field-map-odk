e04f9dbeb71668060c6336459b4564567a9ec697
ivangayton <ivangayton@gmail.com>
2022-09-08 21:40:04 +0900

    Added some QR code research

--- odk_fieldmap/odk_requests.py
+++ odk_fieldmap/odk_requests.py
@@ -47,16 +47,13 @@ r.json()[0]['name']
 import sys, os
 import requests
 import json
-import zlib
+
+# QR code stuff
+from base64 import b64encode
 import codecs
-import urllib
-
-general = {
-    "form_update_mode": "match_exactly",
-    "autosend": "wifi_and_cellular",
-    "basemap_source": "mapbox",
-}
-
+import json
+import segno
+import zlib
 
 def projects(base_url, aut):
     """Fetch a list of projects on an ODK Central server."""
@@ -149,7 +146,9 @@ def app_users(base_url, aut, pid):
     url = (f'{base_url}/v1/projects/{pid}/app-users')
     return requests.get(url, auth=aut)
 
+#################################################
 # POST
+#################################################
 
 def create_project(base_url, aut, project_name):
     """Create a new project on an ODK Central server"""
@@ -175,30 +174,6 @@ def update_role_app_user(base_url, aut, pid, fid, actorId, roleId=2):
     url = (f'{base_url}/v1/projects/{pid}/forms/{fid}/'
            f'assignments/{roleId}/{actorId}')
     return requests.post(url, auth=aut)
-
-def give_access_app_users(base_url, aut, pid, roleId=2):
-    """Give all the app-users in the project access to all the forms in that project"""
-    url = f'{base_url}/v1/projects/{pid}/forms'
-    forms = requests.get(url, auth=aut).json()
-
-    for form in forms:
-        formId = form['xmlFormId']
-        url = f'{base_url}/v1/projects/{pid}/app-users'
-        app_users = requests.get(url, auth=aut).json()
-        for user in app_users:
-            kwargs = {
-                "formId": formId,
-                "actorId": user['id'],
-            }
-            update_role_app_user(base_url, aut, pid, roleId=roleId, **kwargs)
-            #url = f'{base_url}/v1/projects/{pid}/forms/{formId}/assignments/{roleId}/{actorId}' 
-            #requests.post(url, auth = aut)
-    return "Role successfully changed", 200
-
-def delete_project(base_url, aut, pid):
-    """Permanently delete project from an ODK Central server. Probably don't."""
-    url = f'{base_url}/v1/projects/{pid}'
-    return requests.delete(url, auth=aut)
 
 def create_form(base_url, aut, pid, name, form):
     """Create a new form on an ODK Central server
@@ -237,51 +212,32 @@ def publish_form(base_url, aut, pid, fid):
            f'{fid}/draft/publish?version=yeahgo')
     return requests.post(url, auth=aut)
 
-def get_qr_code(base_url, aut, pid, token, admin={}, general=general):
-    url = f'{base_url}/v1/key/{token}/projects/{pid}'
-    qr_data = {
-        "general": general,
-        "admin": admin,
-    }
-    qr_data['general']['server_url'] = url
-    qr_data_bytes = json.dumps(qr_data).encode('utf-8') 
-    qr_data_comp = zlib.compress(qr_data_bytes)
-    qr_data_comp_utf = codecs.encode(qr_data_comp, 'base64_codec') 
-    qr_data_comp_str = qr_data_comp_utf.decode('utf-8').replace('\n', '')
-    img = qrcode.make(qr_data_comp_str)
-    img.save("check_this.png")
-    return img, 200
-
-def generate_qr_data_dict(base_url, aut, pid, admin={}, general=general):
-    url = f'{base_url}/v1/projects/{pid}/app-users'
-    app_users = requests.get(url, auth=aut).json()
-    qr_data_dict = {}
-    false = False
-    true = True
-    for app_user in app_users:
-        token = app_user['token']
-        app_user_id = app_user['id']
-        url = f'{base_url}/v1/key/{token}/projects/{pid}'
-        qr_data = {
-            "general": general,
-            "admin": admin,
+def qr_code():
+    """
+    Based on the info at 
+    https://docs.getodk.org/collect-import-export/
+    """
+    general = {
+        "form_update_mode": "match_exactly",
+        "autosend": "wifi_and_cellular",
+        "basemap_source": "mapbox",
         }
-        qr_data['general']['server_url'] = url
-        qr_data_bytes = json.dumps(qr_data).encode('utf-8') 
-        qr_data_comp = zlib.compress(qr_data_bytes)
-        qr_data_comp_utf = codecs.encode(qr_data_comp, 'base64_codec') 
-        qr_data_comp_str = qr_data_comp_utf.decode('utf-8').replace('\n', '')
-        img = qrcode.make(qr_data_comp_str)
-        img.save('MyQRCode1.png') 
-        qr_data_dict[app_user_id] = qr_data_comp_str
+    
+    settings = { ... }
+    
+    qr_data = (b64encode(zlib.compress(json.dumps(settings)
+                                       .encode("utf-8"))))
+    code = segno.make(qr_data, micro=False)
+    code.save('settings.png', scale=5)
 
-    return (json.dumps(qr_data_dict), 200)
+def delete_project(base_url, aut, pid):
+    """Permanently delete project from an ODK Central server. Probably don't."""
+    url = f'{base_url}/v1/projects/{pid}'
+    return requests.delete(url, auth=aut)
 
-
-# Test QR settings data
-# See here to see all the possible settings: https://docs.getodk.org/collect-import-export/?highlight=configure
-
+###########################################
 
 if __name__ == '__main__':
+    # For now it is a library for other modules to use
     pass
 
